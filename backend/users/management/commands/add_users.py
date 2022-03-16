3dacb7b198fa8408b13bfa0a858fd63ebc36cb73
Ivan Chaplinskiy <chaplinskiy@ya.ru>
2022-03-16 18:38:13 +0300

    fucking with users

--- backend/users/management/commands/add_users.py
+++ backend/users/management/commands/add_users.py
@@ -16,8 +16,7 @@ class Command(BaseCommand):
                 if len(row) == 5:
                     User.objects.get_or_create(
                         username=row[0],
-                        # password=row[1],
-                        password=User.set_password(row[1]),
+                        password=User.set_password(self, raw_password=row[1]),
                         email=row[2],
                         first_name=row[3],
                         last_name=row[4]
